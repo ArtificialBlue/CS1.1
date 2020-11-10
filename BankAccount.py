3317ac8d7796dd9daa3a3110e01a045da80f58cf
ArtificialBlue <oceantraveler201@gmail.com>
2020-11-10 12:31:16 -0800

    First Account Tested

--- BankAccount.py
+++ BankAccount.py
@@ -1,13 +1,16 @@
 class BankAccount:
     def __init__(self,full_name,account_number,routing_number,balance):
         self.full_name = full_name
+        #Randomly generated 8 digit number, unique per account.
         self.account_number = account_number
+        #9 digit number, this number is the same for all accounts.
         self.routing_number = routing_number
+        #The balance of money in the account, should start at 0.
         self.balance = balance
 
     def deposit(self,amount):
         self.balance += amount
-        print(f"Amount Deposited: ${amount}")
+        print("Amount Deposited: $" + str(amount) )
 
     def withdraw(self, amount):
         if self.balance >= amount:
@@ -28,8 +31,18 @@ class BankAccount:
 
     def print_receipt(self):
         print(self.full_name)
-        print("Account No.: ****" + self.account_number[4:])
-        print("Routing No.: " + self.routing_number)
-        print("Balance: $" + self.balance)
+        print("Account No.: ****" + str(self.account_number)[4:])
+        print("Routing No.: " + str(self.routing_number))
+        print("Balance: $" + str(self.balance))
+
+Account1 = BankAccount("Alexander Lopez",19471347,205214952,0.0)
+Account2 = BankAccount("Jeff Bezos",13471947,205214952,0.0)
+Account3 = BankAccount("Elon Musk",13321921,205214952,0.0)
+
+Account1.print_receipt()
+Account1.deposit(1000000)
+Account1.withdraw(100)
+Account1.print_receipt()
+
 
 
