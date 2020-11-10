6d16da38a35fed5faed53a232a4ddaed597d604a
ArtificialBlue <oceantraveler201@gmail.com>
2020-11-09 19:46:37 -0800

    Attributes and Methods

---
+++ BankAccount.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self,full_name,account_number,routing_number,balance):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
+
+    def deposit(amount):
+        pass
+
+    def withdraw(amount):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def add_interest(self):
+        pass
+
+    def print_receipt(self):
+        pass
